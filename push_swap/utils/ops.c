abbbe74487798a6b3aeaa6a90028d5580f7954b9
dreywesson <dreywesson@gmail.com>
2023-03-22 15:02:21 +0100

    double swap operation implemented

--- push_swap/utils/ops.c
+++ push_swap/utils/ops.c
@@ -6,12 +6,12 @@
 /*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 13:54:56 by doduwole          #+#    #+#             */
-/*   Updated: 2023/03/22 14:50:14 by doduwole         ###   ########.fr       */
+/*   Updated: 2023/03/22 15:02:07 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
-// swap
+
 void	swap_nodes(t_node* a, t_node* b)
 {
 	int	tmp;
@@ -20,7 +20,13 @@ void	swap_nodes(t_node* a, t_node* b)
 	a->content = b->content;
 	b->content = tmp;
 }
-// double swap
+
+void	double_swap(t_node* a, t_node* b, t_node* x, t_node* y)
+{
+	swap_nodes(a, b);
+	swap_nodes(x, y);
+}
+
 // push
 // rotate
 // double rotate
