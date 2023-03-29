35acce93db14c21cf90c81b02f7730061939e5be
dreywesson <dreywesson@gmail.com>
2023-03-29 23:56:12 +0100

    reduced if statement

--- push_swap/sort.c
+++ push_swap/sort.c
@@ -6,7 +6,7 @@
 /*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 23:42:08 by doduwole          #+#    #+#             */
-/*   Updated: 2023/03/29 23:35:47 by doduwole         ###   ########.fr       */
+/*   Updated: 2023/03/29 23:55:58 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void sort_three_max(t_node** head_ref)
 {
-	int* ptr;
 	int mid_pos;
 	int max_pos;
 	int min_pos;
+	int* ptr;
 
 	if (is_sorted(head_ref))
 		return;
@@ -32,9 +32,7 @@ void sort_three_max(t_node** head_ref)
 		rotate(head_ref, "ra");
 	else if (max_pos > mid_pos)
 		swap_nodes(*head_ref, (*head_ref)->next, "sa");
-	else if (min_pos < mid_pos && max_pos == mid_pos)
-		reverse_rotate(head_ref, "rra");
-	else if (max_pos == mid_pos && min_pos > mid_pos)
+	else if ((min_pos < mid_pos || min_pos > mid_pos))
 		reverse_rotate(head_ref, "rra");
 	sort_three_max(head_ref);
 }
