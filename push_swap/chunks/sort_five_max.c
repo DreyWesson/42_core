6bc3ad8d9a57894d9e06b60bbe8a7558da815ec5
dreywesson <dreywesson@gmail.com>
2023-04-09 19:27:22 +0100

    dummy move resolved

--- push_swap/chunks/sort_five_max.c
+++ push_swap/chunks/sort_five_max.c
@@ -6,7 +6,7 @@
 /*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 18:56:40 by doduwole          #+#    #+#             */
-/*   Updated: 2023/04/09 16:09:42 by doduwole         ###   ########.fr       */
+/*   Updated: 2023/04/09 19:17:40 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,28 @@ int fastforward(t_node* min_node, t_node** stack_b, char* found, t_node** stack_
 		tmp = tmp->next;
 	}
 	(void)stack_a;
-	// printf("fastforward: %d, found: %c, b-top: %d\n", size, *found, (*stack_b)->value);
 	// ft_print_nodes(stack_a);
+	// printf("fastforward: %d, found: %c, b-top: %d\n", size, *found, (*stack_b)->value);
 	return (size);
 }
+
+int rest_node(t_node** head_ref, int num)
+{
+	t_node* tmp;
+	int size;
+
+	if (!head_ref)
+		return (0);
+	size = 0;
+	tmp = *head_ref;
+	while (tmp && tmp->value != num)
+	{
+		size++;
+		tmp = tmp->next;
+	}
+	return (size);
+}
+
 int rewind_node(t_node* min_node, t_node** stack_b, char* found, t_node** stack_a)
 {
 	t_node* tmp;
@@ -54,8 +72,8 @@ int rewind_node(t_node* min_node, t_node** stack_b, char* found, t_node** stack_
 		tmp = tmp->prev;
 	}
 	(void)stack_a;
+	ft_print_nodes(stack_a);
 	// printf("rewind: %d, found: %c, b-top: %d\n", size, *found, (*stack_b)->value);
-	// ft_print_nodes(stack_a);
 	return (size);
 }
 
@@ -64,15 +82,10 @@ void move_picker(t_node** stack, int target_pos, int mid_pos)
 	int size;
 
 	size = lst_size(stack);
-	// printf("target_pos: %d, mid_pos: %d\n", target_pos, mid_pos);
 	if (target_pos <= mid_pos)
-	{
 		repeat_rotate(stack, target_pos, "ra");
-	}
 	else if (target_pos > mid_pos)
-	{
 		repeat_reverse(stack, size - target_pos, "rra");
-	}
 }
 
 void pusher(t_node** stack_a, t_node** stack_b)
@@ -89,18 +102,16 @@ void pusher(t_node** stack_a, t_node** stack_b)
 	pos += fastforward(min->node, stack_b, &found, stack_a);
 	if (min->node->prev && found != 'y')
 		pos += rewind_node(min->node, stack_b, &found, stack_a);
+	else if (min->node->prev && found == 'y')
+		pos += rest_node(stack_a, min->value);
 	if (found == 'y')
-	{
 		move_picker(stack_a, pos, mid->pos);
-	}
 	else
 	{
 		if ((*stack_b)->value > max->value || (*stack_b)->value < min->value)
-		{
 			move_picker(stack_a, min->pos, mid->pos);
-		}
-		push(stack_b, stack_a, "pa");
 	}
+	push(stack_b, stack_a, "pa");
 	// ft_print_nodes(stack_a);
 }
 
@@ -112,6 +123,7 @@ void re_sort(t_node** stack_a)
 	min = min_node_details(stack_a);
 	mid = mid_node_details(stack_a);
 	move_picker(stack_a, min->pos, mid->pos);
+
 }
 
 void sort_more(t_node** stack_a, t_node** stack_b, int threshold_num)
